98a40eccb2c141460da7811b61808051785a748a
Panther1986 <polzovatel77@gmail.com>
2024-04-25 19:47:15 +0300

    update5

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,49 +1,70 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { Formik, Form, Field } from "formik";
+import { ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { addContact } from "../../redux/contacts/operations";
 import css from "./ContactForm.module.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const FeedbackSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  usernumber: Yup.string().min(3, "Too Short!").required("Required"),
+});
+
+const initialValues = {
+  username: "",
+  usernumber: "",
+};
+
 const ContactForm = () => {
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const dispatch = useDispatch();
+  const dispach = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name.trim() === "" || phoneNumber.trim() === "") {
-      toast.error("Name and phone number are required.");
-    } else {
-      dispatch(addContact({ name, phoneNumber }));
-      setName("");
-      setPhoneNumber("");
-    }
+  const handleSubmit = (values, actions) => {
+    dispach(
+      addContact({
+        name: values.username,
+        number: values.usernumber,
+      })
+    );
+    actions.resetForm();
   };
-
   return (
-    <div>
-      <ToastContainer />
-      <form onSubmit={handleSubmit} className={css.formContainer}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-          className={css.fieldForm}
-        />
-        <input
-          type="text"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          placeholder="Phone Number"
-          className={css.fieldForm}
-        />
-        <button type="submit" className={css.btnForm}>
-          Add Contact
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
+      <Form className={css.formContainer}>
+        <div className={css.labelContainer}>
+          <label className={css.labelForm}>Name</label>
+          <Field className={css.fieldForm} type="text" name="username" />
+          <ErrorMessage
+            className={css.error}
+            name="username"
+            component="span"
+          />
+        </div>
+
+        <div className={css.labelContainer}>
+          <label className={css.labelForm}>Number</label>
+          <Field className={css.fieldForm} type="number" name="usernumber" />
+          <ErrorMessage
+            className={css.error}
+            name="usernumber"
+            component="span"
+          />
+        </div>
+
+        <button className={css.btnForm} type="submit">
+          Add contact
         </button>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   );
 };
 
