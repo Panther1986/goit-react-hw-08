6d35661774cdb29a42bdcb0f301bed4d1cc411ee
Olha Meshcheriakova <polzovatel77@gmail.com>
2024-07-17 23:33:07 +0300

    update13

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 
-// axios.defaults.baseURL = "https://connections-api.herokuapp.com";
-axios.defaults.baseURL = "http://localhost:3002";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+// axios.defaults.baseURL = "http://localhost:3002";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
